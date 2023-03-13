fd21ab9b6e092c5c648e2ab20b825a9570a084a6
Luan Victor de Araujo Gomes <80120493+GomesLuan@users.noreply.github.com>
2023-03-13 15:59:51 -0300

    Construtores + named par + arrow func

--- dart1.dart
+++ dart1.dart
@@ -1,52 +1,14 @@
-import 'dart:io';
 import 'dart:core';
 
 void main() {
-  Produto prod1 = Produto();
-  prod1.descricao = 'X-Tudo';
-  prod1.preco = 12;
-  Produto prod2 = Produto();
-  prod2.descricao = 'Batata Frita';
-  prod2.preco = 7;
-  Produto prod3 = Produto();
-  prod3.descricao = '1L de Pitu';
-  prod3.preco = 15;
-  List<Produto> produtos = [prod1, prod2, prod3];
-  String? opcao_venda;
-  String? opcao_item;
-  do {
-    List<Item> itens = [];
-    do {
-      cardapio();
-      print("\nEscolha uma opção: ");
-      int? prod = int.parse(stdin.readLineSync()!);
-      print("Quantas unidades deseja adicionar? ");
-      int? quant = int.parse(stdin.readLineSync()!);
-      Item item = Item();
-      item.produto = produtos[prod-1];
-      item.quant = quant;
-      print("\nItem adicionado à sacola!");
-      itens.add(item);
-      print("Quer adicionar mais itens? (s/n): ");
-      opcao_item = stdin.readLineSync();
-    } while (opcao_item == 's'); 
-    Venda venda = Venda();
-    venda.data = '${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}';
-    venda.itens = itens;
-    detalhesVenda(venda);
-    print("\nQuer realizar uma nova venda? (s/n): ");
-    opcao_venda = stdin.readLineSync();
-  } while (opcao_venda == 's');
-}
-
-void cardapio() {
-  print('########################################');
-  print('#           C A R D Á P I O            #');
-  print('########################################');
-  print('#     1 - X-Tudo (R\$ 12.00)            #');
-  print('#     2 - Batata Frita (R\$ 7.00)       #');
-  print('#     3 - 1L de Pitu (R\$ 15.00)        #');
-  print('########################################');
+  Produto prod1 = Produto(descricao: 'X-tudo', preco:12);
+  Produto prod2 = Produto(descricao: 'Batata Frita', preco:7);
+  Produto prod3 = Produto(descricao: '1L de cana', preco:15);
+  Item item1 = Item(produto:prod1, quant:2);
+  Item item2 = Item(produto:prod2, quant:3);
+  Item item3 = Item(produto:prod3, quant:1);
+  Venda venda = Venda(data:'${DateTime.now()}', itens:[item1, item2, item3]);
+  detalhesVenda(venda);
 }
 
 void detalhesVenda(Venda venda) {
@@ -60,26 +22,26 @@ void detalhesVenda(Venda venda) {
 }
 
 class Venda {
-  late String data;
-  late List<Item> itens;
+  String data;
+  List<Item> itens;
   
-  double total() {
-    double soma = itens.map((item) => item.total()).reduce((v, e) => v + e);
-    return soma;
-  }
+  double total() => itens.map((item) => item.total()).reduce((v, e) => v + e);
+
+  Venda({required this.data, required this.itens});
 }
 
 class Item {
-  late int quant;
-  late Produto produto;
+  Produto produto;
+  int quant;
   
-  double total(){
-    double preco = produto.preco;
-    return preco*quant;
-  }
+  double total() => quant*produto.preco;
+
+  Item({required this.produto, required this.quant});
 }
 
 class Produto {
-  late String descricao;
-  late double preco;
+  String descricao;
+  double preco;
+
+  Produto({required this.descricao, required this.preco});
 }
